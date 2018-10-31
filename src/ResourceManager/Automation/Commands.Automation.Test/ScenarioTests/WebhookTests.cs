086cb261a91ca49e9fdb578c2f10ac43ea9ce557
Mike Richmond <mirichmo@microsoft.com>
2018-10-31 13:45:52 -0700

    Update webhook tests

--- src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/WebhookTests.cs
+++ src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/WebhookTests.cs
@@ -12,7 +12,7 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-namespace Commands.Automation.Test
+namespace CAT //Commands.Automation.Test
 {
     using Microsoft.Azure.Commands.Automation.Test;
     using Microsoft.WindowsAzure.Commands.ScenarioTest;
@@ -29,140 +29,49 @@ namespace Commands.Automation.Test
             XunitTracingInterceptor.AddToContext(logger);
         }
 
-        [Fact(Skip = "Test needs to be re-recorded.")]
+        //[Fact(Skip = "Test needs to be re-recorded.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         [Trait(Category.Service, Category.Automation)]
-        public void CreateWindowsOneTimeSUCWithDefaults()
+        public void BasicWebhookCrud()
         {
-            RunPowerShellTest(logger, "Test-CreateWindowsOneTimeSoftwareUpdateConfigurationWithDefaults");
+            RunPowerShellTest(logger, "Test-BasicWebhookCrud");
         }
 
-        [Fact(Skip = "Test needs to be re-recorded.")]
+        //[Fact(Skip = "Test needs to be re-recorded.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         [Trait(Category.Service, Category.Automation)]
-        public void CreateLinuxOneTimeSUCWithDefaults()
+        public void NewAutomationWebhookWithParameters()
         {
-            RunPowerShellTest(logger, "Test-CreateLinuxOneTimeSoftwareUpdateConfigurationWithDefaults");
+            RunPowerShellTest(logger, "Test-NewAutomationWebhookWithParameters");
         }
 
-        [Fact(Skip = "Test needs to be re-recorded.")]
+        //[Fact(Skip = "Test needs to be re-recorded.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         [Trait(Category.Service, Category.Automation)]
-        public void CreateWindowsOneTimeSUCWithAllOption()
+        public void WebhookCreateFailsIfMandatoryParametersNotSpecified()
         {
-            RunPowerShellTest(logger, "Test-CreateWindowsOneTimeSoftwareUpdateConfigurationWithAllOption");
+            RunPowerShellTest(logger, "Test-WebhookCreateFailsIfMandatoryParametersNotSpecified");
         }
 
-        [Fact(Skip = "Test needs to be re-recorded.")]
+        //[Fact(Skip = "Test needs to be re-recorded.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         [Trait(Category.Service, Category.Automation)]
-        public void CreateLinuxOneTimeSUCWithAllOption()
+        public void GetWebhookSuccessScenarios()
         {
-            RunPowerShellTest(logger, "Test-CreateLinuxOneTimeSoftwareUpdateConfigurationWithAllOption");
+            RunPowerShellTest(logger, "Test-GetWebhookSuccessScenarios");
         }
 
-        [Fact(Skip = "Test needs to be re-recorded.")]
+        //[Fact(Skip = "Test needs to be re-recorded.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         [Trait(Category.Service, Category.Automation)]
-        public void CreateLinuxOneTimeSUCNonAzureOnly()
+        public void GetWebhookFailureScenarios()
         {
-            RunPowerShellTest(logger, "Test-CreateLinuxOneTimeSoftwareUpdateConfigurationNonAzureOnly");
-        }
-
-        [Fact(Skip = "No recording generated")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void CreateLinuxOneTimeSUCNoTarget()
-        {
-            RunPowerShellTest(logger, "Test-CreateLinuxOneTimeSoftwareUpdateConfigurationNoTargets");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllSUCs()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateConfigurations");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllSUCsForVM()
-        {
-            RunPowerShellTest(logger, "Test-GetSoftwareUpdateConfigurationsForVM");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void DeleteSUC()
-        {
-            RunPowerShellTest(logger, "Test-DeleteSoftwareUpdateConfiguration");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllRuns()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateRuns");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllRunsWithFilters()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateRunsWithFilters");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllRunsWithFiltersNoResults()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateRunsWithFiltersNoResults");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllMachineRuns()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateMachineRuns");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllMachineRunsWithFilters()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateMachineRunsWithFilters");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void GetAllMachineRunsWithFiltersNoResults()
-        {
-            RunPowerShellTest(logger, "Test-GetAllSoftwareUpdateMachineRunsWithFiltersNoResults");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void CreateLinuxWeeklySUC()
-        {
-            RunPowerShellTest(logger, "Test-CreateLinuxWeeklySoftwareUpdateConfiguration");
-        }
-
-        [Fact(Skip = "Test needs to be re-recorded.")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Service, Category.Automation)]
-        public void CreateWindowsMonthlySUC()
-        {
-            RunPowerShellTest(logger, "Test-CreateWindowsMonthlySoftwareUpdateConfiguration");
+            RunPowerShellTest(logger, "Test-GetWebhookFailureScenarios");
         }
     }
 }
